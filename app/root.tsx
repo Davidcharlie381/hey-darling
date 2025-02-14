a53bd2684d7480d91890f967b97a55d0009463a8
Charles <98989744+Davidcharlie381@users.noreply.github.com>
2025-02-14 20:40:20 +0100

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -18,7 +18,7 @@ export const links: LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=Syne:wght@400..800&display=swap",
   },
 ];
 
@@ -31,7 +31,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body className="bg-rose-100 h-screen w-screen">
+      <body className="bg-rose-100 h-screen w-screen" style={{fontFamily: "Syne"}}>
         {children}
         <ScrollRestoration />
         <Scripts />
